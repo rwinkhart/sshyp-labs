fd5266351e86bf267f4ce7b427308769cf0f1923
Randall <idgr@tutanota.com>
2022-08-08 01:12:30 -0400

    password-pasture - Initial demo of entry selector

--- password-pasture/build.sh
+++ password-pasture/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-rm -rf cmake-build-debug CMakeFiles cmake_install.cmake CMakeCache.txt Makefile
+rm -rf CMakeFiles cmake_install.cmake CMakeCache.txt Makefile
 cmake ./
 sleep 10  # prevents clock skew
 make
